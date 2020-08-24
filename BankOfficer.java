a5a60827fdd50f0775d003aa4de9fc2553f48b8c
lehoangtuanbk@gmail.com <tuanlh@toshiba-tsdv.com>
2020-08-24 16:36:13 +0700

    update lai officers

--- BankOfficer.java
+++ BankOfficer.java
@@ -5,16 +5,15 @@ public class BankOfficer {
     private String password;
     private String name;
     private int id;
-    public boolean checkBankOfficer(String username, String password){
-        if(username == this.username && password==this.password)
-        {
-            return true;
-
-        }
-        else
-        {
-            return false;
-        }
+    public String getBankOfficer(){
+        return this.id+" "+this.name+""+this.username+""+this.password;
+    }
+    public boolean setBankOfficer(int id, String username, String password, String name){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        return true;
     }
     public String getAllAccont(){
         return "true";
@@ -24,6 +23,9 @@ public class BankOfficer {
     }
     public boolean editAccount(){
         return true;
+    }
+    public String findAccount(){
+        return "true";
     }
     public boolean deleteAccount(){
         return true;
--- Test.java
+++ Test.java
@@ -1,30 +1,50 @@
 package toshibaTraining.Oop;
 
+import java.util.LinkedList;
+import java.util.List;
 
-import java.util.Scanner;
+
+class Student {
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", name=" + name + "]";
+    }
+}
 
 public class Test {
+    public static final int NUM_OF_ELEMENT = 5;
     public static void main(String[] args) {
         // write your code here
-        try {
-            Scanner myScan = new Scanner(System.in);
-            System.out.println("=====================Bank Account Management System=============================");
-            System.out.println("Chon chuc nang: \n1:Bank officer\n2:Customer\n3:Exit");
-            int number = myScan.nextInt();
-            if (number == 1) {
-                System.out.println(number);
-
-            } else if (number == 2) {
-                System.out.println(number);
-
-            } else if (number == 3) {
-                System.out.println("Chuong trinh ket thuc");
-                System.exit(0);
-            }
-        }
-        catch (Exception e){
-            System.out.println("Hay nhap tu 1-3");
-        }
-
+        // Create list
+//        List<Student> students = new LinkedList<>();
+//        for (int i = 1; i <= NUM_OF_ELEMENT; i++) {
+//            // Add element to list
+//            Student student = new Student(i, "myname" + i);
+//            students.add(student);
+//        }
+//
+//        // Show list student
+//        for (Student student : students) {
+//            System.out.println(student);
+//        }
+        // khai báo 1 LinkedList có tên là list
+        // có kiểu là String
+        LinkedList<String> list = new LinkedList<String>();
+        // thêm các phần tử vào list
+        list.add("Java");
+        list.add("C++");
+        list.add("PHP");
+        list.add("Java");
+        // hiển thị các phần tử của list
+        System.out.println("Các phần tử có trong list là: ");
+        System.out.println(list);
     }
 }
